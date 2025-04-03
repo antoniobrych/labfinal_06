598350dbd9ba7595b49a85bed4a6a16692dc49ed
Antonio Brych <63557494+antoniobrych@users.noreply.github.com>
2025-04-03 13:01:43 -0300

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<svelte:head>
+  <title>Mike Wazowski: Personal site and portfolio</title>
+</svelte:head>
+<script>
+  import projects from "$lib/projects.json";
+  import Project from "$lib/Project.svelte";
+</script>
+<h1> Mike Wazowski</h1>
+   
+<img src="./images/mike.png" alt="mike" width="500px">
+
+<p>Michael "Mike" Thomas Wazowski is the deuteragonist of the 2001 Disney Pixar animated film Monsters, Inc. and the protagonist of its 2013 prequel.
+    Depicted as a diminutive, one-eyed monster with a wisecracking veneer, Mike is an employee of Monsters, Incorporated, where he works closely with his longtime partner/best friend Sulley.
+    Mike's world gets turned upside down when a human girl (nicknamed "Boo") enters the monster world.
+    Teaming up with Sulley to return Boo to her world, Mike uncovers a company conspiracy and helps solve an energy crisis that plagues the entire city of Monstropolis
+</p>
+<h2>
+  Latest Projects
+</h2>
+<div class="projects">
+{#each projects.slice(0, 3) as p}
+  <Project data={p} hLevel="3"/>
+{/each}
+</div>
+<p>Hello Wordl ! Wellcome to my site
+My name is Antonio, I am a Data Science student</p>
+
+{#await fetch("https://api.github.com/users/antoniobrych")}
+  <p>Loading...</p>
+{:then response}
+  {#await response.json()}
+    <p>Decoding...</p>
+  {:then data} 
+    <section>
+      <h2>My Github Stats</h2>
+      <dl>
+        <dt>Followers</dt>
+        <dd>{data.followers}</dd>
+        <dt>Following</dt>
+        <dd>{data.following}</dd>
+        <dt>Public Repos</dt>
+        <dd>{data.public_repos}</dd>
+      </dl>
+    </section>
+  {:catch error}
+    <p class="error">Something went wrong: {error.message}</p>
+  {/await}
+  {:catch error}
+    <p class="error">Something went wrong: {error.message}</p>
+{/await}
